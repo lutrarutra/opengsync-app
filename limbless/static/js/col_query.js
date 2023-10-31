68f9a12f436f71ce1420ec6378dd6a43cae3e87e
lutrarutra <artur.gynter@proton.me>
2023-10-31 18:23:23 +0100

    fixed query col

--- limbless/static/js/col_query.js
+++ limbless/static/js/col_query.js
@@ -1,13 +1,13 @@
-var selected_column_uuid = null;
+var selected_column_target = null;
 var selected_column_var = null;
 
 function show_query_col() {
-    const w = $(`#${selected_column_uuid}+${selected_column_var}`).width();
-    const h = $(`#${selected_column_uuid}+${selected_column_var}`).height();
+    const w = $(`#${selected_column_target}\\:${selected_column_var}`).width();
+    const h = $(`#${selected_column_target}\\:${selected_column_var}`).height();
     $(".temp-query-input").hide();
     $(".query-col").show();
-    $(`#${selected_column_uuid}+${selected_column_var}`).hide();
-    $(`#${selected_column_uuid}+${selected_column_var}-temp`).show().css({
+    $(`#${selected_column_target}\\:${selected_column_var}`).hide();
+    $(`#${selected_column_target}\\:${selected_column_var}-temp`).show().css({
         "width": `${w}px`,
     }).children("input").css({
         "width": `${w}px`,
@@ -19,10 +19,12 @@ $(document).off("contextmenu", ".query-col").on("contextmenu", ".query-col", fun
     e.preventDefault();
     const x = e.pageX;
     const y = e.pageY;
-    selected_column_uuid = e.target.id.split("+")[0];
-    selected_column_var = e.target.id.split("+")[1];
-    console.log(selected_column_uuid);
-    console.log(selected_column_var);
+
+    const target_id = $(e.target).closest("th").attr("id");
+
+    selected_column_target = target_id.split(":")[0];
+    selected_column_var = target_id.split(":")[1];
+    
     $("#copy-edit-menu").css({
         display: "block",
         left: x + 8, top: y + 8
