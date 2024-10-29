699f08748676ce9f535e43c7b3db16901f7020ef
Artur Gynter <agynter@lbi-dev.int.cemm.at>
2024-10-29 08:24:31 +0100

    postgres conf mount

--- prod.sh
+++ prod.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-if [ ! -f "./db/postgres/postgresql.conf" ]; then
-    echo "Copying templates/postgres/postgresql.conf to db/postgres/"
-    mkdir -p ./db/postgres
-    cp ./templates/postgres/postgresql.conf ./db/postgres/
-fi
-
 if [ ! -f "rclone/rclone.conf" ]; then
     echo "Error: You must copy templates/rclone/rclone.conf to rclone/ and populate it."
     exit 1
