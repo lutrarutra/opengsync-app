8d1a93b6b0ad52f08755b1d7e1bfc62973acc7ae
lutrarutra <artur.gynter@proton.me>
2025-01-09 11:21:23 +0100

    rclone -> mounting remote drives in host machine

--- services/limbless-app/entrypoint.sh
+++ services/limbless-app/entrypoint.sh
@@ -7,12 +7,6 @@ if [ $? -eq 1 ]; then
     python3 init_db.py
 fi
 
-export TENX_MNT_POINT="/usr/src/app/mnt/sequences_10x"
-mkdir -p $TENX_MNT_POINT
-echo "Mounting: ${TENX_SEQUENCES_FOLDER} -> $TENX_MNT_POINT"
-rclone mount cemm_cluster:${TENX_SEQUENCES_FOLDER} $TENX_MNT_POINT --dir-cache-time 5s \
-    --poll-interval 10s --vfs-cache-mode full --read-only --cache-dir=/data/cache/0 --allow-non-empty --allow-other --daemon --log-file=/usr/src/app/logs/rclone.log
-
 if [ $? -eq 0 ]; then
     python3 /usr/src/app/debug.py --host=${LIMBLESS_HOST} --port=${LIMBLESS_PORT}
 else
