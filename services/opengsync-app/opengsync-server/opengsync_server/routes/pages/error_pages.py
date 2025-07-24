00cf8adfcce4c48c3b30397c26afa34e9b72ce01
lutrarutra <artur.gynter@proton.me>
2025-07-24 13:27:17 +0200

    added dbsession to error pages

--- services/opengsync-app/opengsync-server/opengsync_server/routes/pages/error_pages.py
+++ services/opengsync-app/opengsync-server/opengsync_server/routes/pages/error_pages.py
@@ -1,18 +1,25 @@
 from flask import Blueprint, render_template
 
+from opengsync_db import db_session
+
+from ... import db
+
 errors_bp = Blueprint('errors_bp', __name__)
 
 
 @errors_bp.app_errorhandler(404)
+@db_session(db)
 def error_404(error):
     return render_template('errors/404.html'), 404
 
 
 @errors_bp.app_errorhandler(403)
+@db_session(db)
 def error_403(error):
     return render_template('errors/403.html'), 403
 
 
 @errors_bp.app_errorhandler(500)
+@db_session(db)
 def error_500(error):
     return render_template('errors/500.html'), 500
