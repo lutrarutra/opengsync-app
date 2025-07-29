fb9e89d160a68ae4604c3dd32da8279193e650fc
lutrarutra <artur.gynter@proton.me>
2025-07-29 12:51:35 +0200

    dilute pools workflow fix

--- services/opengsync-app/opengsync-server/opengsync_server/forms/workflows/dilute_pools/DilutePoolsForm.py
+++ services/opengsync-app/opengsync-server/opengsync_server/forms/workflows/dilute_pools/DilutePoolsForm.py
@@ -23,35 +23,34 @@ class DilutePoolsForm(HTMXFlaskForm):
     target_total_volume = FloatField(validators=[DataRequired()], default=50)
     target_molarity = FloatField("Target Molarity", validators=[DataRequired()], default=3.0)
 
-    def __init__(self, formdata: dict = {}):
+    def __init__(self, experiment: models.Experiment, formdata: dict = {}):
         HTMXFlaskForm.__init__(self, formdata=formdata)
         self._context["warning_min"] = models.Pool.warning_min_molarity
         self._context["warning_max"] = models.Pool.warning_max_molarity
         self._context["error_min"] = models.Pool.error_min_molarity
         self._context["error_max"] = models.Pool.error_max_molarity
         self._context["enumerate"] = enumerate
+        self.experiment = experiment
+        self.df = db.get_experiment_pools_df(experiment.id)
         
-    def prepare(self, experiment: models.Experiment) -> dict:
-        df = db.get_experiment_pools_df(experiment.id)
+    def prepare(self):
+        self.df["molarity"] = self.df["qubit_concentration"] / (self.df["avg_fragment_size"] * 660) * 1_000_000
+        self.df["molarity_color"] = "cemm-green"
+        self.df.loc[(self.df["molarity"] < models.Pool.warning_min_molarity) | (models.Pool.warning_max_molarity < self.df["molarity"]), "molarity_color"] = "cemm-yellow"
+        self.df.loc[(self.df["molarity"] < models.Pool.error_min_molarity) | (models.Pool.error_max_molarity < self.df["molarity"]), "molarity_color"] = "cemm-red"
 
-        df["molarity"] = df["qubit_concentration"] / (df["avg_fragment_size"] * 660) * 1_000_000
-        df["molarity_color"] = "cemm-green"
-        df.loc[(df["molarity"] < models.Pool.warning_min_molarity) | (models.Pool.warning_max_molarity < df["molarity"]), "molarity_color"] = "cemm-yellow"
-        df.loc[(df["molarity"] < models.Pool.error_min_molarity) | (models.Pool.error_max_molarity < df["molarity"]), "molarity_color"] = "cemm-red"
-
-        for i in range(df.shape[0]):
+        for i in range(self.df.shape[0]):
             if i > len(self.input_fields) - 1:
                 self.input_fields.append_entry()
 
-        return {"df": df, "enumerate": enumerate}
+        self._context["df"] = self.df
+        self._context["enumerate"] = enumerate
     
-    def process_request(self, experiment: models.Experiment, user: models.User) -> Response:
-        df = db.get_experiment_pools_df(experiment.id)
-
+    def process_request(self, user: models.User) -> Response:
         if not self.validate():
-            return self.make_response(df=df)
+            return self.make_response(df=self.df)
         
-        for i, (_, row) in enumerate(df.iterrows()):
+        for i, (_, row) in enumerate(self.df.iterrows()):
             entry = self.input_fields[i]
             if entry.qubit_after_dilution.data is None:
                 continue
@@ -64,4 +63,4 @@ class DilutePoolsForm(HTMXFlaskForm):
             db.dilute_pool(pool_id, entry.qubit_after_dilution.data, user.id)
 
         flash("Dilution successful!", "success")
-        return make_response(redirect=url_for("experiments_page.experiment", experiment_id=experiment.id))
+        return make_response(redirect=url_for("experiments_page.experiment", experiment_id=self.experiment.id))
