f5939dc5f3ebd359c3260688a7519d447de4b475
lutrarutra <artur.gynter@proton.me>
2025-07-31 12:54:18 +0200

    rf scanner fix

--- services/opengsync-app/opengsync-server/opengsync_server/routes/pages/error_pages.py
+++ services/opengsync-app/opengsync-server/opengsync_server/routes/pages/error_pages.py
@@ -1,18 +1,23 @@
 from flask import Blueprint, render_template
 
-errors_bp = Blueprint('errors_bp', __name__)
+from ... import db, page_route
+
+errors_bp = Blueprint("errors_bp", __name__)
 
 
 @errors_bp.app_errorhandler(404)
+@page_route(errors_bp, db=db, login_required=False)
 def error_404(error):
     return render_template('errors/404.html'), 404
 
 
 @errors_bp.app_errorhandler(403)
+@page_route(errors_bp, db=db, login_required=False)
 def error_403(error):
     return render_template('errors/403.html'), 403
 
 
 @errors_bp.app_errorhandler(500)
+@page_route(errors_bp, db=db, login_required=False)
 def error_500(error):
     return render_template('errors/500.html'), 500
