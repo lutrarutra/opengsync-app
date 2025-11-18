dc920f997ff894582e59fe7486087f4cc3cda092
lutrarutra <artur.gynter@proton.me>
2025-11-18 13:20:09 +0100

    data path api changes

--- services/opengsync-app/opengsync-server/opengsync_server/routes/api/shares.py
+++ services/opengsync-app/opengsync-server/opengsync_server/routes/api/shares.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 from flask import Blueprint, jsonify
 
-from opengsync_db.categories import DataPathType
+from opengsync_db.categories import DataPathType, DataPathTypeEnum
 
 
 from ...core import wrappers, exceptions, runtime
@@ -24,12 +24,10 @@ def get_real_path(share_path: str) -> str | None:
         if share_path.startswith(key):
             return share_path.replace(key, prefix, 1)
     return None
-    
 
-@wrappers.api_route(shares_api_bp, db=db, methods=["POST"], json_params=["project_id", "path", "path_type_id"])
-def add_data_path_to_project(project_id: int, path: str, path_type_id: int = 0):
+
+def resolve_share_path(path: str, path_type_id: int) -> tuple[str, DataPathTypeEnum]:
     path = Path(path).resolve().as_posix()
-
     try:
         path_type = DataPathType.get(path_type_id)
     except ValueError:
@@ -37,9 +35,6 @@ def add_data_path_to_project(project_id: int, path: str, path_type_id: int = 0):
     
     if not Path(path).is_absolute():
         raise exceptions.BadRequestException(f"Path '{path}' is not an absolute path.")
-    
-    if (project := db.projects.get(project_id)) is None:
-        raise exceptions.NotFoundException(f"Project with ID '{project_id}' not found.")
     
     if (share_path := get_share_path(path)) is None:
         raise exceptions.BadRequestException(f"Invalid share path '{path}'. Path must start with one of: {', '.join(runtime.app.share_path_mapping.values())}")
@@ -53,39 +48,131 @@ def add_data_path_to_project(project_id: int, path: str, path_type_id: int = 0):
     except ValueError:
         raise exceptions.BadRequestException(f"Path '{path}' is outside of share root.")
     
-    share_path = p.relative_to(runtime.app.share_root).as_posix()
-
-    logger.debug(share_path)
-
     if p.is_dir() and path_type != DataPathType.DIRECTORY:
         raise exceptions.BadRequestException(f"Path '{path}' is a directory, but path type is not DIRECTORY.")
     
     if not p.is_dir() and path_type == DataPathType.DIRECTORY:
         raise exceptions.BadRequestException(f"Path '{path}' is not a directory, but path type is DIRECTORY.")
     
-    if len(project_data_paths := db.data_paths.find(path=share_path, project_id=project.id)[0]) > 0:
-        data_path = project_data_paths[0]
-        data_path.type = path_type
-        db.data_paths.update(data_path)
-    else:
-        data_path = db.data_paths.create(
-            path=share_path,
-            type=path_type,
-            project=project,
-        )
+    return p.relative_to(runtime.app.share_root).as_posix(), path_type
+    
+
+@wrappers.api_route(shares_api_bp, db=db, methods=["POST"], json_params=["project_id", "seq_request_id", "experiment_id", "library_id", "path", "path_type_id"])
+def add_data_path(
+    path: str, path_type_id: int,
+    seq_request_id: int | None = None,
+    project_id: int | None = None,
+    experiment_id: int | None = None,
+    library_id: int | None = None,
+):
+    share_path, path_type = resolve_share_path(path, path_type_id)
+    
+    if project_id is not None:
+        if (project := db.projects.get(project_id)) is None:
+            raise exceptions.NotFoundException(f"Project with ID '{project_id}' not found.")
+        if len(project_data_paths := db.data_paths.find(path=share_path, project_id=project.id)[0]) > 0:
+            data_path = project_data_paths[0]
+            data_path.type = path_type
+            db.data_paths.update(data_path)
+        else:
+            data_path = db.data_paths.create(
+                path=share_path,
+                type=path_type,
+                project=project,
+            )
+        
+    if seq_request_id is not None:
+        if (seq_request := db.seq_requests.get(seq_request_id)) is None:
+            raise exceptions.NotFoundException(f"Seq Request with ID '{seq_request_id}' not found.")
+        
+        if len(seq_request_data_paths := db.data_paths.find(path=share_path, seq_request_id=seq_request.id)[0]) > 0:
+            data_path = seq_request_data_paths[0]
+            data_path.type = path_type
+            db.data_paths.update(data_path)
+        else:
+            data_path = db.data_paths.create(
+                path=share_path,
+                type=path_type,
+                seq_request=seq_request,
+            )
+
+    if experiment_id is not None:
+        if (experiment := db.experiments.get(experiment_id)) is None:
+            raise exceptions.NotFoundException(f"Experiment with ID '{experiment_id}' not found.")
+        
+        if len(experiment_data_paths := db.data_paths.find(path=share_path, experiment_id=experiment.id)[0]) > 0:
+            data_path = experiment_data_paths[0]
+            data_path.type = path_type
+            db.data_paths.update(data_path)
+        else:
+            data_path = db.data_paths.create(
+                path=share_path,
+                type=path_type,
+                experiment=experiment,
+            )
+
+    if library_id is not None:
+        if (library := db.libraries.get(library_id)) is None:
+            raise exceptions.NotFoundException(f"Library with ID '{library_id}' not found.")  
+        if len(library_data_paths := db.data_paths.find(path=share_path, library_id=library.id)[0]) > 0:
+            data_path = library_data_paths[0]
+            data_path.type = path_type
+            db.data_paths.update(data_path)
+        else:
+            data_path = db.data_paths.create(
+                path=share_path,
+                type=path_type,
+                library=library,
+            )
 
     return jsonify({"result": "success", "share_path": share_path, "path": path, "type": path_type.name}), 200
 
 @wrappers.api_route(shares_api_bp, db=db, methods=["DELETE"], json_params=["project_id"])
-def remove_data_paths_from_project(project_id: int):    
-    if (project := db.projects.get(project_id)) is None:
-        raise exceptions.NotFoundException(f"Project with ID '{project_id}' not found.")
-    
-    data_paths, _ = db.data_paths.find(project_id=project.id)
+def remove_data_paths(
+    seq_request_id: int | None = None,
+    project_id: int | None = None,
+    experiment_id: int | None = None,
+    library_id: int | None = None
+):    
     paths = []
-    for data_path in data_paths:
-        db.data_paths.delete(data_path)
-        exists = (runtime.app.share_root / data_path.path).exists()
-        paths.append((data_path.path, get_real_path(data_path.path), exists))
+    if project_id is not None:
+        if (project := db.projects.get(project_id)) is None:
+            raise exceptions.NotFoundException(f"Project with ID '{project_id}' not found.")
+        
+        data_paths, _ = db.data_paths.find(project_id=project.id)
+        for data_path in data_paths:
+            db.data_paths.delete(data_path)
+            exists = (runtime.app.share_root / data_path.path).exists()
+            paths.append((data_path.path, get_real_path(data_path.path), exists))
+
+    if seq_request_id is not None:
+        if (seq_request := db.seq_requests.get(seq_request_id)) is None:
+            raise exceptions.NotFoundException(f"Seq Request with ID '{seq_request_id}' not found.")
+        
+        data_paths, _ = db.data_paths.find(seq_request_id=seq_request.id)
+        for data_path in data_paths:
+            db.data_paths.delete(data_path)
+            exists = (runtime.app.share_root / data_path.path).exists()
+            paths.append((data_path.path, get_real_path(data_path.path), exists))
+
+    if experiment_id is not None:
+        if (experiment := db.experiments.get(experiment_id)) is None:
+            raise exceptions.NotFoundException(f"Experiment with ID '{experiment_id}' not found.")
+        
+        data_paths, _ = db.data_paths.find(experiment_id=experiment.id)
+        for data_path in data_paths:
+            db.data_paths.delete(data_path)
+            exists = (runtime.app.share_root / data_path.path).exists()
+            paths.append((data_path.path, get_real_path(data_path.path), exists))
+
+    if library_id is not None:
+        if (library := db.libraries.get(library_id)) is None:
+            raise exceptions.NotFoundException(f"Library with ID '{library_id}' not found.")  
+        
+        data_paths, _ = db.data_paths.find(library_id=library.id)
+        for data_path in data_paths:
+            db.data_paths.delete(data_path)
+            exists = (runtime.app.share_root / data_path.path).exists()
+            paths.append((data_path.path, get_real_path(data_path.path), exists))
     
     return jsonify({"result": "success", "paths": paths}), 200
