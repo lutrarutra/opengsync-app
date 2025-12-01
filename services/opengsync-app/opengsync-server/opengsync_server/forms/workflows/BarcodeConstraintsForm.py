d5de26e8b88fac8607890d90275723b0b1e43390
lutrarutra <artur.gynter@proton.me>
2025-12-01 10:45:33 +0100

    index constraints form fixes

--- services/opengsync-app/opengsync-server/opengsync_server/forms/workflows/BarcodeConstraintsForm.py
+++ services/opengsync-app/opengsync-server/opengsync_server/forms/workflows/BarcodeConstraintsForm.py
@@ -63,13 +63,16 @@ class BarcodeConstraintsForm(HTMXFlaskForm):
         self.needed_additions_i7: list[list[Index]] = []
         self.needed_additions_i5: list[list[Index]] = []
         self.needed_additions: list[list[DualIndex]] = []
+        self.additional_sequences: list[tuple[str | None, str | None]] = []
+        self.needed_bases = ["T", "C"]
+        self.active_tab = "form-tab-form"
 
     def validate(self) -> bool:
         if not super().validate():
             return False
-        
-        additional_i7: list[str] = []
-        additional_i5: list[str] = []
+                
+        kit_sequences_i7: list[str] = []
+        kit_sequences_i5: list[str] = []
         additional_sequences: list[str] = []
         kit = None
         
@@ -95,9 +98,9 @@ class BarcodeConstraintsForm(HTMXFlaskForm):
             kit_i7_len = len(barcodes_df["sequence_i7"].values[0])
             kit_i5_len = len(barcodes_df["sequence_i5"].values[0])
             
-            additional_i7 = barcodes_df["sequence_i7"].to_list()
-            additional_i5 = barcodes_df["sequence_i5"].to_list()
-            additional_sequences = [s1 + s2 for s1, s2 in zip(additional_i7, additional_i5)]
+            kit_sequences_i7 = barcodes_df["sequence_i7"].to_list()
+            kit_sequences_i5 = barcodes_df["sequence_i5"].to_list()
+            additional_sequences = [s1 + s2 for s1, s2 in zip(kit_sequences_i7, kit_sequences_i5)]
                         
         if not self.spreadsheet.validate():
             return False
@@ -115,6 +118,21 @@ class BarcodeConstraintsForm(HTMXFlaskForm):
         sequences_i7 = [s for s in df["sequence_i7"] if pd.notna(s) and s]
         sequences_i5 = [s for s in df["sequence_i5"] if pd.notna(s) and s]
         
+        for i in range(max(len(sequences_i7), len(sequences_i5))):
+            if i < len(sequences_i7):
+                seq_i7 = sequences_i7[i]
+            else:
+                seq_i7 = None
+            if i < len(sequences_i5):
+                seq_i5 = sequences_i5[i]
+            else:
+                seq_i5 = None
+
+            self.additional_sequences.append((seq_i7, seq_i5))
+
+        self.active_tab = "sequences-tab-form"
+            
+        # No additional sequences provided
         if not sequences_i7 and not sequences_i5:
             if not additional_sequences:
                 self.spreadsheet.add_general_error("Select an index kit or provide some barcodes")
@@ -130,22 +148,22 @@ class BarcodeConstraintsForm(HTMXFlaskForm):
                         seq_i5 = s[kit_i7_len:]  # type: ignore
                         res.append(
                             DualIndex(
-                                name_i7=mapping.at[seq_i7, "name_i7"],
-                                name_i5=mapping.at[seq_i7, "name_i5"],
-                                well=mapping.at[seq_i7, "well"],
+                                name_i7=mapping.at[seq_i7, "name_i7"],  # type: ignore
+                                name_i5=mapping.at[seq_i7, "name_i5"],  # type: ignore
+                                well=mapping.at[seq_i7, "well"],  # type: ignore
                                 sequence_i7=seq_i7,
                                 sequence_i5=seq_i5
                             )
                         )
                     self.needed_additions.append(res)
-
+        # Dual Index
         elif sequences_i7 and sequences_i5:
             if kit is not None:
                 if kit.type != IndexType.DUAL_INDEX:
                     self.spreadsheet.add_general_error(f"The selected kit '{kit.name}' is not a dual index kit.")
                     return False
                 
-                additional_sequences = [s1 + s2 for s1, s2 in zip(additional_i7, additional_i5)]
+                additional_sequences = [s1 + s2 for s1, s2 in zip(kit_sequences_i7, kit_sequences_i5)]
             else:
                 additional_sequences = []
                 
@@ -157,6 +175,7 @@ class BarcodeConstraintsForm(HTMXFlaskForm):
             if not utils.check_index_constraints(sequences):
                 if not additional_sequences:
                     self.spreadsheet.add_general_error("Index constraints not met. Select another kit.")
+                    self.kit.selected.errors = ["Index constraints not met. Select another kit."]
                 else:
                     needed_additions = utils.generate_valid_combinations(sequences, additional_indices=additional_sequences, min_samples=self.min_samples.data)
                     mapping = barcodes_df.set_index("sequence_i7")  # type: ignore
@@ -167,50 +186,51 @@ class BarcodeConstraintsForm(HTMXFlaskForm):
                             seq_i5 = s[kit_i7_len:]  # type: ignore
                             res.append(
                                 DualIndex(
-                                    name_i7=mapping.at[seq_i7, "name_i7"],
-                                    name_i5=mapping.at[seq_i7, "name_i5"],
-                                    well=mapping.at[seq_i7, "well"],
+                                    name_i7=mapping.at[seq_i7, "name_i7"],  # type: ignore
+                                    name_i5=mapping.at[seq_i7, "name_i5"],  # type: ignore
+                                    well=mapping.at[seq_i7, "well"],  # type: ignore
                                     sequence_i7=seq_i7,
                                     sequence_i5=seq_i5
                                 )
                             )
                         self.needed_additions.append(res)
+        # Single Index (i7 or i5)
         else:
             if sequences_i7 and not utils.check_index_constraints(sequences_i7):
-                if not additional_i7:
+                if not kit_sequences_i7:
                     self.spreadsheet.add_general_error("i7 index constraints not met. Select kit another kit.")
                 else:
-                    needed_additions_i7 = utils.generate_valid_combinations(sequences_i7, additional_indices=additional_i7)
+                    needed_additions_i7 = utils.generate_valid_combinations(sequences_i7, additional_indices=kit_sequences_i7)
                     for a in needed_additions_i7:
                         res = []
                         for s in a:
                             mapping = barcodes_df.set_index("sequence_i7")  # type: ignore
                             res.append(
                                 Index(
-                                    name=mapping.at[s, "name_i7"],
-                                    well=mapping.at[s, "well"],
+                                    name=mapping.at[s, "name_i7"],  # type: ignore
+                                    well=mapping.at[s, "well"],  # type: ignore
                                     sequence=s
                                 )
                             )
+                        self.needed_additions_i7.append(res)
                     
-
             if sequences_i5 and not utils.check_index_constraints(sequences_i5):
-                if not additional_i5:
+                if not kit_sequences_i5:
                     self.spreadsheet.add_general_error("i5 index constraints not met. Select kit another kit.")
                 else:
-                    needed_additions_i5 = utils.generate_valid_combinations(sequences_i5, additional_indices=additional_i5)
+                    needed_additions_i5 = utils.generate_valid_combinations(sequences_i5, additional_indices=kit_sequences_i5)
                     for a in needed_additions_i5:
                         res = []
                         for s in a:
                             mapping = barcodes_df.set_index("sequence_i5")  # type: ignore
                             res.append(
                                 Index(
-                                    name=mapping.at[s, "name_i5"],
-                                    well=mapping.at[s, "well"],
+                                    name=mapping.at[s, "name_i5"],  # type: ignore
+                                    well=mapping.at[s, "well"],  # type: ignore
                                     sequence=s
                                 )
                             )
-        
+                        self.needed_additions_i5.append(res)
         return True
 
     def process_request(self) -> Response:
