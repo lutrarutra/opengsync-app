505a23dd1de6353907cddd8037d5ca96b71ea602
lutrarutra <artur.gynter@proton.me>
2026-09-25 14:26:06 +0200

    fixes

--- services/backend/server/forms/workflows/share_project_data/__init__.py
+++
@@ -1,7 +0,0 @@
-from .ShareProjectDataWorkflow import ShareProjectDataWorkflow
-from .ShareProjectDataForm import ShareProjectDataForm
-
-__all__ = [
-    "ShareProjectDataWorkflow",
-    "ShareProjectDataForm",
-]
